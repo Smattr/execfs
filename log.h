d1adcfa74385c3ee719afe1fa8885cd22a15f713
Matthew Fernandez <matthew.fernandez@gmail.com>
2012-09-02 18:02:08 +1000

    Nicer wrapper for logging.

--- log.h
+++ log.h
@@ -5,6 +5,13 @@ int log_open(char *filename);
 void log_close(void);
 void log_write(char *format, ...);
 
+/* Swap the comments below to switch log information between standard logging
+ * and code debugging.
+ */
+#define LOG(format, args...) \
+    log_write("%s:%d:%s(): " format, __FILE__, __LINE__, __func__ , ## args)
+//#define LOG(format, args...) log_write(format , ## args)
+
 extern FILE *log_file;
 
 #endif

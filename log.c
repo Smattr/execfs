54c20d0fbdc579fbc266f6efe8e0cfef43d70596
Matthew Fernandez <matthew.fernandez@gmail.com>
2012-09-02 18:01:43 +1000

    Timestamp and flush log entries.

--- log.c
+++ log.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "globals.h"
 
@@ -21,9 +22,17 @@ void log_close(void) {
 
 void log_write(char *format, ...) {
     if (log_file != NULL) {
+        /* Print timestamp */
+        time_t tt = time(NULL);
+        struct tm *t = localtime(&tt);
+        fprintf(log_file, "[%02d-%02d-%04d %02d:%02d:%02d] ", t->tm_mday,
+            t->tm_mon, t->tm_year + 1900, t->tm_hour, t->tm_min, t->tm_sec);
+
         va_list ap;
         va_start(ap, format);
         (void)vfprintf(log_file, format, ap);
         va_end(ap);
+        fprintf(log_file, "\n");
+        fflush(log_file);
     }
 }

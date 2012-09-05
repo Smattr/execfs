0b3de063ee45a5bcec7055d419e409a2bfa6ffd1
Matthew Fernandez <matthew.fernandez@gmail.com>
2012-09-05 18:38:03 +1000

    Add test for reading a file.

---
+++ tests/test-read.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Test reading from an execfs file.
+
+if [ $# -ne 1 ]; then
+    echo $#
+    echo "Usage: $0 mountpoint" >&2
+    exit 1
+fi
+
+OUTPUT=`cat "$1/file"`
+if [ $? -ne 0 ]; then
+    echo "Failed to read from file." >&2
+    exit 1
+elif [ "${OUTPUT}" != "hello world" ]; then
+    echo "Incorrect output received." >&2
+    exit 1
+fi
---
+++ tests/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Mount an execfs file system, run a test on it, unmount it and delete the
+# mount point.
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 script config" >&2
+    exit 1
+fi
+
+MOUNT=`mktemp -d`
+execfs --config "$2" --fuse "${MOUNT}" && \
+ "$1" "${MOUNT}" && \
+ fusermount -u "${MOUNT}" && \
+ rm -rf "${MOUNT}"
